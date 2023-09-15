2dbe2d6a1e46a5598667d3589d3f0a88ad57683d
Chinazaekpere Uchenna <111239582+overdose-dev@users.noreply.github.com>
2023-09-15 23:05:26 +0100

    Add files via upload
    uploaded main files such as css, html and js files along with image folder

---
+++ script.js
@@ -0,0 +1,30 @@
+const emailInput = document.getElementById("email_input");
+const invalidEmail = document.querySelector(".invalid");
+const submitButton = document.querySelector("button");
+
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const email = emailInput.value.trim();
+
+  if (!email) {
+    // If the email field is empty
+    emailInput.style.border = "2px solid #ff5476";
+    invalidEmail.style.display = "block";
+  } else if (!isValidEmail(email)) {
+    // If the email is not properly formatted
+    emailInput.style.border = "2px solid #ff5476";
+    invalidEmail.style.display = "block";
+  } else {
+    // If the email is valid
+    emailInput.style.border = "2px solid silver";
+    invalidEmail.style.display = "none";
+    window.location.href = `success.html?email=${encodeURIComponent(email)}`;
+  }
+});
+
+// Function to validate email format using a simple regex pattern
+function isValidEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
